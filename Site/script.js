4db41b8c3e6d79a26954d3a1d4aebbc44350bec6
gustavo tessitore <gugatessi@gmail.com>
2018-10-25 11:24:59 -0300

    firebase

--- Site/script.js
+++ Site/script.js
@@ -19,27 +19,35 @@ document.addEventListener('DOMContentLoaded', function() {
     A função extract lê a query string da página atual e
     transforma ela em um dicionário JavaScript usual.
   */
-  //let params = extract()
+  let params = extract()
 
   /*
     Se não sabe o que é isto, volte ao ingrediente 3.
   */
 
-  //let path = '/' + params['category'] + '/courses/' + params['course']
-  let path = '/' 
+  let path = '/' + params['category'] + '/courses/' + params['course']
+
+  console.log(params)
+
+    if(Object.keys(params).length === 0){
+      path = '/'
+    }
+    else{
+      path = '/' + params['category'] + '/Courses/' + params['course']
+    }
+  console.log(path)
+  //let path = '/' 
   db.download(path, function(data) {
     /*
       Se não sabe o que é isto, volte ao ingrediente 3.
-
-
     */
-
-
-
-    console.log(data['Habilidades'])
+    if(Object.keys(params).length === 0){
     replace('body', {
         'goals': data['Habilidades'],
         'cursos': data['Habilidades']['Apresentação']['Cursos']
-    })
+    })}
+    else{
+      console.log(path)
+    }
   })
 })
