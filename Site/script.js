eef8229ec700c451c8af94d9060493c27be2b239
gustavo tessitore <gugatessi@gmail.com>
2018-10-26 09:35:31 -0300

    adição de imagens e remoçao de paginas

--- Site/script.js
+++ Site/script.js
@@ -1,71 +1,37 @@
-<<<<<<< HEAD
-/*
-  Se não sabe o que é isto, volte para os vídeos de JavaScript.
-*/
+
 document.addEventListener('DOMContentLoaded', function() {
-  /*
-    Se não sabe o que é isto, volte ao ingrediente 2.
-  */
-  //let db = connect('https://codesign-classic-7b2bb.firebaseio.com')
 
   let db = connect('https://site-insper.firebaseio.com/')
 
-
-  /*
-    Uma query string é uma maneira de colocar um dicionário
-    no próprio endereço da página. O início dela é marcado
-    por um "?" e ela consiste em um número indeterminado de
-    pares "chave=valor" separados por "&".
-
-    A função extract lê a query string da página atual e
-    transforma ela em um dicionário JavaScript usual.
-  */
   let params = extract()
 
-  /*
-    Se não sabe o que é isto, volte ao ingrediente 3.
-  */
-
-  let path = '/' + params['category'] + '/courses/' + params['course']
+  let path = '/Habilidades/' + params['category'] + '/Cursos/' + params['course']
 
   console.log(params)
 
     if(Object.keys(params).length === 0){
       path = '/'
     }
-    else{
-      path = '/' + params['category'] + '/Courses/' + params['course']
-    }
+    
   console.log(path)
-  //let path = '/' 
+  
   db.download(path, function(data) {
-    /*
-      Se não sabe o que é isto, volte ao ingrediente 3.
-    */
+
+    
+    console.log(data)
     if(Object.keys(params).length === 0){
-    replace('body', {
+       
+      replace('body', {
         'goals': data['Habilidades'],
         'cursos': data['Habilidades']['Apresentação']['Cursos']
-    })}
-    else{
-      console.log(path)
+      })
     }
+    else{
+      
+      replace('body', {
+        'cursos': data
+      })
+    }
+
   })
 })
-=======
-document.addEventListener('DOMContentLoaded', function() {
-	let db = connect('https://site-insper.firebaseio.com/')
-
-	let params = extract()
-
-	let url = '/' + params['category'] + '/courses/' + params['course']
-
-	db.download(url, function(data) {
-		replace('body', {
-		'name': data['name'],
-		'descricao': data['descricao'],
-		'hab': data['hab']
-		})
-	})
-})
->>>>>>> c636944ab70fadc841a6a86090e29314881de4b8
